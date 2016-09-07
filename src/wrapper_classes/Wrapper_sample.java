8df25a72f0a1e4ab487196559e599e10471dc8a1
Pratik Joshi <pratsj9@live.in>
2016-09-07 09:16:44 +0530

    wrapper updates

--- src/wrapper_classes/Wrapper_sample.java
+++ src/wrapper_classes/Wrapper_sample.java
@@ -1,3 +1,5 @@
+
+//Practicing some wrapper classes and passing some default and incorrect inputs to them to see how they print their output
 package wrapper_classes;
 
 public class Wrapper_sample {
@@ -11,6 +13,18 @@ public static void main(String args[])
 	System.out.println("\n\tPrinting wrapper Integer Object:"+i1);
 	int p1 = i1;
 	System.out.println("\n\tPrinting wrapper Integer to Variable Conversion:"+p1);
+	
+	
+	System.out.println("\n\t Printing Float object:");
+	Float f1 = new Float(45);    //Notice that an integer value has been passed but it'll convert into a float
+	System.out.println("\n\tFloat object value:"+f1);
+	
+	System.out.println("\n\t Printing Boolean value:");
+	Boolean b1 = new Boolean(true);
+	System.out.println("\n\tBoolean Object Value:"+b1);
+	b1 = new Boolean("xyz");  		//This time a string value has been passed that is not true/false
+	System.out.println("\n\t Boolean Object Value(String):"+b1);
+	
 
 	
 
