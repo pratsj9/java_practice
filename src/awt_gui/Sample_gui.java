833bebc7cee11aeb199bf6f68f23b8c69fcd8714
Pratik Joshi <pratsj9@live.in>
2016-09-14 10:53:56 +0530

    AWT basics

--- src/awt_gui/Sample_gui.java
+++ src/awt_gui/Sample_gui.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+
 @SuppressWarnings("serial")
 public class Sample_gui extends Frame  {
 	
@@ -14,44 +16,52 @@ public class Sample_gui extends Frame  {
 		final TextField t1 = new TextField();
 		final TextField t2 = new TextField();
 		final TextField t3= new TextField();
+		Label username = new Label();
+		Label passwd = new Label();
 		Button b1 = new Button();
+		
+		
 		b1.setLabel("Click!");
+		username.setText("Username");
+		passwd.setText("Password");
+		
+		
 		b1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				t3.setText(t1.getText() +""+ t2.getText());
 				
-				t3.setText(t1.getText()+" "+t2.getText());
-				
-			
-			}
-		});
+										
+				}});
 		
+		//Frame and TextField Setup
 		f1.setSize(650, 650);
-		b1.setBounds(80,40,50,50);
-		t1.setBounds(150, 40,120, 100);
-		t2.setBounds(150, 140, 120,100);
-		t3.setBounds(150, 240, 120,100);
+		b1.setBounds(120,400,50,50);
 		
+		username.setBounds(50, 50, 120, 30);		
+		passwd.setBounds(50, 100,120, 30);
+		t1.setBounds(200,50, 120, 30);
+		t2.setBounds(200,100, 120,30);
+		t3.setBounds(250, 150, 120,30);
+		
+		
+		
+		//Adding all components to Frame
+		f1.add(username);
+		f1.add(passwd);
 		f1.add(b1);
 		f1.add(t1);
 		f1.add(t2);
 		f1.add(t3);
+		
+		//Set Frame Layout and make it visible
 		f1.setLayout(null);
 		f1.setVisible(true);
-		
-		
-		
+			
 	}
 
 	
 	
 	
 	public static void main(String[] args) {
-		
-
-	new Sample_gui();
-	}
-
-
-
-}
+		new Sample_gui();}}
