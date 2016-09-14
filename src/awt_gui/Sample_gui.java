3fe3274a3c0b4e5db1ee128bad874c0514d18fb6
Pratik Joshi <pratsj9@live.in>
2016-09-14 10:20:10 +0530

    AWT basics

--- src/awt_gui/Sample_gui.java
+++ src/awt_gui/Sample_gui.java
@@ -1,10 +1,57 @@
 package awt_gui;
 
-public class Sample_gui {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+@SuppressWarnings("serial")
+public class Sample_gui extends Frame  {
+	
+	
+	public Sample_gui() 
+	{
+		final Frame f1 = new Frame();
+		final TextField t1 = new TextField();
+		final TextField t2 = new TextField();
+		final TextField t3= new TextField();
+		Button b1 = new Button();
+		b1.setLabel("Click!");
+		b1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				t3.setText(t1.getText()+" "+t2.getText());
+				
+			
+			}
+		});
+		
+		f1.setSize(650, 650);
+		b1.setBounds(80,40,50,50);
+		t1.setBounds(150, 40,120, 100);
+		t2.setBounds(150, 140, 120,100);
+		t3.setBounds(150, 240, 120,100);
+		
+		f1.add(b1);
+		f1.add(t1);
+		f1.add(t2);
+		f1.add(t3);
+		f1.setLayout(null);
+		f1.setVisible(true);
+		
+		
+		
 	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+	new Sample_gui();
+	}
+
+
 
 }
